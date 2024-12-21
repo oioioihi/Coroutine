7f7377f85be870da6d8e34db7ad9bd412f011fce
Boeun's <rnqhdms@gmail.com>
2024-12-22 00:13:27 +0900

    Dispatcher.Default

--- src/main/kotlin/section2/Code2-2.kt
+++ src/main/kotlin/section2/Code2-2.kt
@@ -5,6 +5,9 @@ import kotlinx.coroutines.*
 /**
  * Dispatchers.IO : 네트워크 요청이나 DB 읽기 쓰기 같은 입출력 (I/O)작업을 실행하느 디스패처
  * 사용할 수 있는 스레드의 수 : 64 와 JVM에서 사용할 수 있는 프로세서의 수 중 큰 값 -> Runtime.getRuntime().availableProcessors()
+ *
+ * Dispatchers.Default : CPU 바운드 작업을 위한 디스패처 -> 이미지, 동영상 처리 같은 끊이지 않고 연산이 필요한 작업
+ * 사용할 수 있는 스레드이 수 : Runtime.getRuntime().availableProcessors()와 2중 큰 값 -> CPU 코어 수와 동일한 크기
  */
 
 
@@ -22,6 +25,19 @@ fun main() = runBlocking<Unit> {
 
         launch {
             println("[${Thread.currentThread().name}] 작업3 실행")
+        }
+    }
+
+    launch(Dispatchers.Default) {
+        launch {
+            println("[${Thread.currentThread().name}] 작업4 실행")
+        }
+
+        launch {
+            println("[${Thread.currentThread().name}] 작업5 실행")
+        }
+        launch {
+            println("[${Thread.currentThread().name}] 작업6 실행")
         }
     }
 }
