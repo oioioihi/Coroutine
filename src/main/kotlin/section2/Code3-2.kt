26deafc0a4039838dc46f789b8d3c4a4940c3483
Boeun's <rnqhdms@gmail.com>
2024-12-23 00:57:17 +0900

    코루틴으 지연 실행

---
+++ src/main/kotlin/section2/Code3-2.kt
@@ -0,0 +1,29 @@
+package section2
+
+import kotlinx.coroutines.*
+
+/**
+ * CoroutineStart.LAZY
+ *
+ */
+
+
+fun main() {
+    runBlocking<Unit> {
+
+        val startTime = System.currentTimeMillis()
+        launch { //launch 함수를 호출해 실행하면, 코루틴이 즉시 실행된다.
+            println("[${getElapsedTime(startTime)}] launch 코루틴 실행")
+        }
+
+        val lazyJob = launch(start = CoroutineStart.LAZY) { //launch 함수의 start인자로 CoroutineStart.LAZY를 넘기면 지연 코루틴이 생성된다.
+            println("[${getElapsedTime(startTime)}] launch 코루틴 지연 실행")
+        }
+
+        lazyJob.start() // 코루틴 실행 -> join() 호출해도 실행됨(join은 실행한 스레드가 블럭됨)
+        println("종료")
+    }
+}
+
+fun getElapsedTime(startTime: Long): String =
+    "지난 시간: ${System.currentTimeMillis() - startTime} 밀리초"
