8e913eb2607cfb1d6c5ea64cfb80513feeb91f35
Boeun's <rnqhdms@gmail.com>
2024-12-26 02:08:27 +0900

    코루틴 > 취소요청 확인 > delay, yield

--- src/main/kotlin/section2/Code3-5.kt
+++ src/main/kotlin/section2/Code3-5.kt
@@ -3,20 +3,30 @@ package section2
 import kotlinx.coroutines.*
 
 /**
+ * 코루틴에 취소 확인 시점을 만드는 세가지 방법
+ *
+ * 1. delay 함수를 사용해 취소 확인 시점 만들기 -> 일시 중단 시점에 코루틴의 취소가 확인돼 취소된다.
+ * 2. yield 함수를 사용해 취소 확인 시점 만들기 -> yield 함수를 호출한 코루틴은 자신이 사용하던 스레드를 양보한다.
+ * delay 함수와 yield함수는 모두 일시중단 후 재개 과정을 거친다. -> 재개 시에는 CoroutineDispatcher에 의해 다시 스레드로 보내지는 과정을 거치기 때문에 비효율적이다.
+ * 3. CoroutineScope.isActive를 사용한 취소 확인
  */
 
 
-fun main() {
-    runBlocking<Unit> {
-
-        val startTime = System.currentTimeMillis()
-        val longJob: Job = launch(Dispatchers.Default) {
-            Thread.sleep(1000L) // 코루틴의 일시 중단 되는 지점이 없기 때문에 결국엔 해당 로직을 모두 실행됨 -> delay(1000L)를 사용하면 일시중단 됨
-            println("longJob 코루틴의 동작")
+fun main() = runBlocking<Unit> {
+    val whileJob = launch(Dispatchers.Default) {
+        while (true) {
+            println("작업 중")
+            yield()
+            /**
+             * 스레드를 양보한다는 것은 코루틴이 스레드 사용을 중단하고, 일시중단 한다는 뜻
+             * 스레드를 양보한 후 곧바로 재개 요청을 한다.
+             */
         }
-
-        longJob.cancelAndJoin() // 취소 요청한 후 취소가 완료될 때까지 호출 코루틴 일시 중단
     }
+
+    delay(100L)
+    whileJob.cancel()
 }
+
 
 
