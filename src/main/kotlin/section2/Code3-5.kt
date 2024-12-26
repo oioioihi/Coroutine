48d8ce8574c87c98756b3de1a81122fc9e805735
Boeun's <rnqhdms@gmail.com>
2024-12-26 11:41:55 +0900

    코루틴 > 취소요청 확인 > CoroutineScope.isActive

--- src/main/kotlin/section2/Code3-5.kt
+++ src/main/kotlin/section2/Code3-5.kt
@@ -8,14 +8,14 @@ import kotlinx.coroutines.*
  * 1. delay 함수를 사용해 취소 확인 시점 만들기 -> 일시 중단 시점에 코루틴의 취소가 확인돼 취소된다.
  * 2. yield 함수를 사용해 취소 확인 시점 만들기 -> yield 함수를 호출한 코루틴은 자신이 사용하던 스레드를 양보한다.
  * delay 함수와 yield함수는 모두 일시중단 후 재개 과정을 거친다. -> 재개 시에는 CoroutineDispatcher에 의해 다시 스레드로 보내지는 과정을 거치기 때문에 비효율적이다.
- * 3. CoroutineScope.isActive를 사용한 취소 확인
+ * 3. CoroutineScope.isActive(확장 프로퍼티)를 사용한 취소 확인 -> 코루틴에 취소가 요청되면, CoroutineScope.isActive가 false가 된다.
  */
 
 
 fun main() = runBlocking<Unit> {
     val whileJob = launch(Dispatchers.Default) {
         while (true) {
-            println("작업 중")
+            println("1작업 중")
             yield()
             /**
              * 스레드를 양보한다는 것은 코루틴이 스레드 사용을 중단하고, 일시중단 한다는 뜻
@@ -23,9 +23,16 @@ fun main() = runBlocking<Unit> {
              */
         }
     }
-
     delay(100L)
     whileJob.cancel()
+    val whileJob2 = launch(Dispatchers.Default) {
+        while (this.isActive) {
+            println("2작업 중")
+        }
+    }
+
+    delay(100L)
+    whileJob2.cancel()
 }
 
 
