ec4cbe90dfe580356b8e2b49343758520c8649c5
Boeun's <rnqhdms@gmail.com>
2024-12-29 23:20:52 +0900

    코루틴 실행환경 상속

--- src/main/kotlin/section7/Code7-1.kt
+++ src/main/kotlin/section7/Code7-1.kt
@@ -6,8 +6,6 @@ import kotlinx.coroutines.runBlocking
 /**
  * 부모 코루틴은 자식 코루틴에게 실행 환경을 상속한다.
  * 부모 코루틴이 자식 코루틴을 생성하면 부모 코루틴의 CoroutineContext가 자식 코루틴에게 전달된다.
- *
- * 자식 코루틴을 생성하는 코루틴 빌더함수에 새로운 CoroutineContext 객체가 전달되면, 부모 코루틴에서 전달 받은 요소들은 덮어 씌워진다.
  */
 
 
@@ -16,6 +14,11 @@ fun main() = runBlocking<Unit> {
     launch(coroutineContext1) { //부모 코루틴 생성
         println("[${Thread.currentThread().name}] 부모 코루틴 실행")
         launch {// 자식 코루틴 생성
+            println("[${Thread.currentThread().name}] 자식 코루틴 실행")
+        }
+
+        //자식 코루틴을 생성하는 코루틴 빌더함수에 새로운 CoroutineContext 객체가 전달되면, 부모 코루틴에서 전달 받은 요소들은 덮어 씌워진다.
+        launch(CoroutineName("child_coroutine")) {// 자식 코루틴 생성
             println("[${Thread.currentThread().name}] 자식 코루틴 실행")
         }
     }
